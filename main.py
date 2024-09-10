d9df75dd89091c3cb3d12aa2518bae075a186f1a
bende.zoltan <8a4FM3zuvB>
2024-09-10 16:29:25 +0200

    Add file reader option

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Solve: Download from a youtube channel playlist
 from pytube import YouTube
 # import ffmpeg
 from moviepy.editor import VideoFileClip, AudioFileClip
@@ -7,26 +8,31 @@ import subprocess
 COMPLETE_PATH = './complete'
 INCOMPLETE_PATH = './incomplete'
 
-# YouTube objektum létrehozása
-YOUTUBE_URL = 'https://www.youtube.com/watch?v=IL1nlWOciL0'
-yt = YouTube(YOUTUBE_URL)
-video_title = yt.title
-
 
 class FileName:
-    def __init__(self, video_title):
-        self.filename = self.create_filename(video_title)
+    def __init__(self, _video_title):
+        self.filename = self.create_filename(_video_title)
         self.filename_video = f"{self.filename}_video.mp4"
         self.filename_audio = f"{self.filename}_audio.mp4"
 
     def create_filename(self, text, max_words=4):
-        # Speciális karakterek eltávolítása
+        # Remove special characters
         text = re.sub(r'[^\w\s]', '', text)
-        # Első max_words számú szó kiválasztása
+        # First max_words finding
         words = text.split()[:max_words]
-        # Szavak összekapcsolása alulvonással
+        # Merge words with underlines
         _filename = '_'.join(words)
         return _filename
+
+
+def read_urls_from_file():
+    _urls = []
+    # Open file and read rows
+    with open('download_list', 'r') as file:
+        # Read all rows, cut spaces, add to list
+        _urls = [line.strip() for line in file.readlines()]
+
+    return _urls
 
 
 def download_video():
@@ -46,6 +52,9 @@ def download_video():
         elif "720p" in available_resolutions:
             print(f"720p elérhető")
             resolution = "720p"
+        elif "720p50" in available_resolutions:
+            print(f"720p50 elérhető")
+            resolution = "720p50"
         else:
             print("Nincs megfelelő formátum")
             exit()
@@ -53,15 +62,15 @@ def download_video():
         stream = yt.streams.filter(res=resolution, file_extension="mp4").first()
 
         if stream:
-            print(f"Videó letöltés indítása: \n{video_title}")
+            print(f"Videó letöltés indítása...")
             stream.download(INCOMPLETE_PATH, filename=f"{filenames.filename_video}")
-            print(f"Videó befejezve, fájlnév: {filenames.filename_video}")
+            print(f"Videó befejezve")
 
         else:
             print("Az 1080p felbontású videó nem érhető el ebben a formátumban.")
 
     except Exception as e:
-        print(f"Hiba történt a letöltés során: {str(e)}")
+        print(f"ERROR with download: {str(e)}")
 
 
 def download_audio():
@@ -71,9 +80,9 @@ def download_audio():
 
         if audio_stream:
             # Letöltés indítása
-            print(f"Hang letöltése indítása: {video_title}")
+            print(f"Hang letöltése indítása...")
             audio_stream.download(INCOMPLETE_PATH, filename=f"{filenames.filename_audio}")
-            print(f"Hang letöltés befejezve:")
+            print(f"Hang letöltés befejezve")
 
         else:
             print("Nem találtunk elérhető audio stream-et.")
@@ -114,7 +123,7 @@ def merge_video_audio_pymovie():
 
         print(f"A fájlok egyesítése sikeres: {output_file}")
     except ffmpeg.Error as e:
-        print(f"Hiba történt a fájlok egyesítése során: {e.stderr.decode('utf-8')}")"""
+        print(f"Hiba történt a fájlok egyesítése során: {e.stderr.decode('utf-8')}")
 
 
 def merge_video_audio(video_file, audio_file, output_file):
@@ -130,13 +139,24 @@ def merge_video_audio(video_file, audio_file, output_file):
         print(f"A fájlok egyesítése sikeres: {output_file}")
     except subprocess.CalledProcessError as e:
         print(f"Hiba történt a fájlok egyesítése során: {e}")
-
+"""
 
 if __name__ == "__main__":
-    filenames = FileName(video_title)
-    download_video()
-    download_audio()
+    urls = read_urls_from_file()
 
-    # MERGE
-    # merge_video_audio_ffmpeg_python()
-    merge_video_audio_pymovie()
+    for url in urls:
+        # CREATE YT OBJECT
+        yt = YouTube(url)
+        video_title = yt.title
+        print(f"\n{video_title}\n{'-' * len(video_title)}")
+
+        # FORMAT FILENAME
+        filenames = FileName(video_title)
+
+        # DOWNLOAD
+        download_video()
+        download_audio()
+
+        # MERGE
+        # merge_video_audio_ffmpeg_python()
+        merge_video_audio_pymovie()
